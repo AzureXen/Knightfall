002fabdd7d1620894965628ac5524921d3629c8a
NhatDang1102 <nezukosama247@gmail.com>
2025-03-01 16:24:27 +0700

    thêm slime

---
+++ Knightfall/Assets/Script/Enemy/NhatDangg/NhatManager.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class NhatManager : EntityManager
+{
+    [SerializeField] private Boolean touchDamageEnabled = true;
+    public int touchDamage = 5;
+    public int touchKnockbackForce = 5;
+    public float touchKnockbackDuration = 1.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void Start()
+    {
+        entityHealth = GetComponent<Health>();
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        entityMovement = GetComponent<NhatMovement>();
+        isKnockingBack = false;
+        takeDamageCooldown = 0.2f;
+        flashDuration = 2.5f;
+        EntityName = "NhatDang01";
+        defaultColor = new Color(1, 1, 1, 1);
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        GameObject target = collision.gameObject;
+        if (target.CompareTag("Player") && touchDamageEnabled)
+        {
+            EntityManager targetManager = target.GetComponent<EntityManager>();
+            targetManager.TakeMeleeHit(touchDamage, transform.position, touchKnockbackForce, touchKnockbackDuration, this);
+        }
+    }
+}
