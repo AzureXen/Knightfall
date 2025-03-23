fcdb9b9be7f4cd00e7d13e40550ee5cdb70a1711
NhatDang1102 <nezukosama247@gmail.com>
2025-03-23 12:58:47 +0700

    dialogue va 2dlightray

--- Knightfall/Assets/Script/Enemy/NhatDangg/NhatManager.cs
+++ Knightfall/Assets/Script/Enemy/NhatDangg/NhatManager.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 
 public class NhatManager : EntityManager
 {
-    [SerializeField] private Boolean touchDamageEnabled = true;
+    [SerializeField] private bool touchDamageEnabled = true;
     public int touchDamage = 5;
     public int touchKnockbackForce = 5;
     public float touchKnockbackDuration = 1.5f;
-    private NhatAttack attackScript;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private NhatAttack attackScript;
+    private EnemyDialogueTrigger dialogueTrigger;
+    private Health healthScript;
+
+    private int lastHealth = -1;
+    private bool isDead = false;
+
     public override void Start()
     {
         base.Start();
         attackScript = GetComponent<NhatAttack>();
-        entityHealth = GetComponent<Health>();
+        dialogueTrigger = GetComponent<EnemyDialogueTrigger>();
+        healthScript = GetComponent<Health>();
+        entityHealth = healthScript;
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         entityMovement = GetComponent<NhatMovement>();
@@ -23,7 +30,27 @@ public class NhatManager : EntityManager
         flashDuration = 2.5f;
         EntityName = "NhatDang01";
         defaultColor = new Color(1, 1, 1, 1);
+
+        lastHealth = healthScript.health;
     }
+
+    private void Update()
+    {
+        if (healthScript == null || isDead) return;
+
+        if (healthScript.health < lastHealth)
+        {
+            dialogueTrigger?.TriggerHitDialogue();
+            lastHealth = healthScript.health;
+        }
+
+        if (healthScript.health <= 0 && !isDead)
+        {
+            isDead = true;
+            dialogueTrigger?.TriggerDeathDialogue();
+        }
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         GameObject target = collision.gameObject;
